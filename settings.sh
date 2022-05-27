eef605dace76e01d41a8fe0c52ac49174c89eaa4
Alex Poon <6593392+ZeroGazer@users.noreply.github.com>
2022-05-27 14:18:59 +0800

    Add power settings

--- settings.sh
+++ settings.sh
@@ -9,3 +9,9 @@ defaults write com.apple.dock autohide-delay -float 0
 
 # Don’t show recent applications in Dock
 defaults write com.apple.dock show-recents -bool false
+
+# Turn off Wake for network access
+sudo systemsetup -setwakeonnetworkaccess off
+
+# Turn on Start up automatically after a power failure
+sudo systemsetup -setrestartpowerfailure off
